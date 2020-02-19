21eefd7e433bdecc69caeb87f34fc5dacc7aa197
braunyman <60123446+braunyman@users.noreply.github.com>
2020-02-19 11:54:48 -0700

    tried merging some code together

--- encoder.ino
+++ encoder.ino
@@ -1,12 +1,42 @@
 #include <Wire.h>
 #include <Encoder.h>
 
-#define ADDRESS 127
-#define RESET 0
-#define GIVE_POS 0
+//i2c handling
+#define ADDRESS 127 //device address
+#define RESET 0 //reset position command
 
-uint32_t pos;
-Encoder Wheel(2,4);
+//wheel schema
+#define CYCLE 0xFF //PWM max
+#define ENABLE 4 //Motor enable pin (RESERVED)
+#define M1DIR 7 //Motor 1 direction
+#define M2DIR 8 //Motor 2 direction
+#define MOTOR1 9
+#define MOTOR2 10
+#define SWITCH 5 //switch enable for motor driver
+#define STATUS 12 //status pin for motor driver
+#define CPR 1600 //counts per rotation
+#define ENC1 2 //encoder pin 1
+#define ENC2 5 //encoder pin 2
+
+//PID control
+#define PCONST 0.32007 
+#define ICONST 0.01120245
+#define DCONST 0
+
+//timing
+#define PERIOD 5 //millisecond delay for 
+
+double currPosition = 0, lastPosition = 0, lastTime = 0; //track historical positions and times for PID control
+double currVel = 0;  //as well as velocity
+// int A[2] = {0, 0};
+// int B[2] = {0, 0};
+// int count = 0;
+int startingTime;
+int period = 5;
+double currTime = 0;
+double voltageCommand, destRads, integral = 0;
+
+Encoder Wheel(ENC1,ENC2);
 
 void setup() {
   Wire.begin(ADDRESS); 
@@ -15,16 +45,54 @@ void setup() {
 
   Serial.begin(9600);
 
-  pos = 0;
-
 }
 
 void loop() {
-  Serial.println(Wheel.read());
-  delay(100);
+//   Serial.println(Wheel.read());
+//   delay(100);
+  
+  currTime = millis();
+  
+  currPosition = Wheel.read() / CPR * 2 * 3.14159; //Current Position in radians
+  currVel = (currPosition - lastPosition) / ((currTime - lastTime)/1000) ; //Velocity in rad/sec
+  lastPosition = currPosition;
+
+  integral += ((currTime - lastTime) / 1000) * (destRads - currPosition) ;
+
+  voltageCommand = (destRads - currPosition) * PCONST + integral * ICONST;
+  
+  Serial.print(voltageCommand);
+  Serial.print("\t");
+  
+  if (voltageCommand < 0){
+    digitalWrite(M1DIR, LOW);
+    voltageCommand *= -1;
+  } else {
+    digitalWrite(M1DIR, HIGH);
+  }
+
+  //analogWrite(MOTOR1, voltageCommand * CYCLE);
+  
+  if (currTime - startingTime < 2000 && currTime - startingTime > 1000){
+    Serial.print(currPosition);
+    Serial.print("\t");
+    Serial.print(currVel);
+    Serial.print("\t");
+    Serial.print(currTime);
+    Serial.print("\t");
+    Serial.print(voltageCommand);
+    Serial.print("\n");
+  }
+  
+  //Serial.print(count);
+  
+  while (millis() < currTime + period);  //Sample every 5ms
+    
+  lastTime = currTime;
 }
 
 void handleRequest() {
+  long long int pos = 0;
   while(1 < Wire.available()) { 
     pos = Wheel.read();
     Wire.beginTransmission(69);
